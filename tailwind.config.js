15350f28b14d81710d0ffe0a14dea9d260723ba9
digitalmaith <digitalmaith@gmail.com>
2026-05-17 14:11:14 +0000

    fix couleur des composant qui était transparent

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,9 +49,20 @@ module.exports = {
           foreground: "hsl(var(--destructive-foreground))",
         },
 
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
+
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
         sans: ["var(--font-inter)", "system-ui", "sans-serif"],
